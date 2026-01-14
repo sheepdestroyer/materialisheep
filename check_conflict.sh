4cf8ed2a8da764ba2eaeb3559bc773429cb5629b
sheepdestroyer <sheepdestroyer@gmail.com>
2026-01-14 20:59:56 +0100

    chore: clean artifacts and upgrade gradle to 9.2.1

--- check_conflict.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "=== GIT STATUS ===" > conflict_status.txt
-git status >> conflict_status.txt 2>&1
-echo "=== GITIGNORE ===" >> conflict_status.txt
-cat .gitignore >> conflict_status.txt
